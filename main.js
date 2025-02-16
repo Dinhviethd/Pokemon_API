d0a21c50dab8a06bcce7f25d96a1279c5b787f30
Dinhviethd <nguyentadinhviet210805@gmail.com>
2025-02-16 10:39:13 +0700

    Push code

--- main.js
+++ main.js
@@ -1,38 +1,59 @@
+const container = document.querySelector(".content");
+const searchBar = document.querySelector(".SearchBar input");
 
-const container= document.querySelector(".content");
-const searchBar_Infor= document.querySelector(".SearchBar input");
-async function loadPokemon(){
+let allPokemon = []; // Lưu danh sách tất cả Pokémon
+
+async function loadPokemon() {
     showLoading();
-    const response= await fetch(`https://pokeapi.co/api/v2/pokemon`);
-    const firstAPI= await response.json(); //ở đây đang có 1 list các object
-    const arr= firstAPI.results;
-    let HTML= "";
-    for (let i=0; i<arr.length; i++){
-        const response2= await fetch(arr[i].url)
-        const info= await response2.json();
-        const typeHTML= info.types.map((typeNames) => {
-            const typeName= typeNames.type.name;
-            return `<span class="Type ${typeName}">${typeName}</span>`
-        }).join("");
-        HTML+= `<div class="Pokemon">
-            <span style="margin-left: 40%">#${info.id} <br></span>
-            <img class="photo"
-                src="${info.sprites.other.home.front_default}"
-                alt="${info.name}">
-            <h3>${info.name}</h3>
-            <div class="Element">
-                ${typeHTML}
-            </div>
-        </div>`;
-        container.innerHTML= HTML;
-    }
-}
-function searchPokemon(){
-    const infor= searchBar_Infor.value;
     
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon`); 
+    const firstAPI = await response.json();
+    const arr = firstAPI.results;
+
+    allPokemon = []; // Xóa danh sách cũ (nếu có)
+
+    for (let i = 0; i < arr.length; i++) {
+        const response2 = await fetch(arr[i].url);
+        const info = await response2.json();
+        allPokemon.push({
+            id: info.id,
+            name: info.name.toLowerCase(),
+            img: info.sprites.other.home.front_default,
+            types: info.types.map(t => t.type.name)
+        });
+    }
+
+    displayPokemon(allPokemon); 
+}
+
+// Hiển thị danh sách Pokémon
+function displayPokemon(pokemonList) {
+    if (pokemonList.length === 0) {
+        container.innerHTML = `<p style="text-align: center; font-size: 20px;">No Pokémon found 😢</p>`;
+        return;
+    }
+
+    container.innerHTML = pokemonList.map(pokemon => {
+        const typeHTML = pokemon.types.map(type => {
+            return `<span class="Type ${type}">${(type)}</span>`;
+        }).join("");
+
+        return `
+        <div class="Pokemon">
+            <span style="margin-left: 40%">#${pokemon.id} <br></span>
+            <img class="photo" src="${pokemon.img}" alt="${pokemon.name}">
+            <h3>${(pokemon.name)}</h3>
+            <div class="Element">${typeHTML}</div>
+        </div>`;
+    }).join("");
+}
+function searchPokemon() {
+    const query = searchBar.value.toLowerCase(); 
+    const filteredPokemon = allPokemon.filter(pokemon => pokemon.name.includes(query));
+    displayPokemon(filteredPokemon); 
 }
 function showLoading() {
-    const loading_screen= "<p>Getting Data From Pokedeck...</p>"
-    container.innerHTML =loading_screen;
+    container.innerHTML = "<p style='text-align:center;'>Getting Data From Pokedeck...</p>";
 }
-loadPokemon();
+searchBar.addEventListener("input", searchPokemon);
+loadPokemon(); 
